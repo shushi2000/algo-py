eea79d102bca0d6da451f395d7cc4f195840976d
Shi <segoviashu2000@yahoo.com>
2019-03-01 16:47:11 -0800

    Update karatsuba_integer_multiplication.py

--- karatsuba_integer_multiplication.py
+++ karatsuba_integer_multiplication.py
@@ -1,7 +1,9 @@
+# The two integers 
 i1 = 3141592653589793238462643383279502884197169399375105820974944592
 i2 = 2718281828459045235360287471352662497757247093699959574966967627
 
 final_result = 0
+
 ip = 0 
 for i in reversed(str(i1)):
     jp = 0
@@ -13,6 +15,7 @@ for i in reversed(str(i1)):
         jp += 1
     ip += 1
 
+# check if the calculation is correct
 assert final_result == i1 * i2
 print ("{} * {} = {}".format(i1, i2, final_result))
 
